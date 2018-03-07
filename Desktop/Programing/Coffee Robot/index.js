7e0fcdef47d25da794a26e0efe4b5b50645161e6
phd-phuoc <36890308+phd-phuoc@users.noreply.github.com>
2018-03-07 16:03:30 +0700

    Add files via upload

--- Desktop/Programing/Coffee Robot/index.js
+++ Desktop/Programing/Coffee Robot/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000);
 
+var order_list=[];
+var order_list_change = 0;
+
 app.use(express.static('RobotCoffe'));
 
 
@@ -12,6 +15,7 @@ var io = socket(server);
 var fs = require('fs');
 var data = fs.readFileSync('data.json');
 var users = JSON.parse(data);
+//console.log(users);
 
 // words.insert = 2;
 // var data = JSON.stringify(words,null,2);
@@ -19,33 +23,94 @@ var users = JSON.parse(data);
 //   console.log(words);
 // });
 
+
 io.sockets.on('connection',function (socket){
   console.log("New client: "+socket.conn.remoteAddress);
 
   socket.on('login',function (data) {
-    console.log(data);
+    //console.log(data);
     var logged_in = 0;
     for (var usr in users){
       if (users[usr].user == data.user && users[usr].pw == data.pw){
         console.log(data.user + " logged in.");
-        console.log(users[usr]);
         logged_in=1;
-        socket.emit('logged-in',users[usr]);
+        users[usr].code = rndStr();
+        if (users[usr].name=="Administrator")
+          socket.emit('admin-logged-in',users[usr]);
+        else
+          socket.emit('user-logged-in',users[usr]);
       }
     }
     if (logged_in==0) socket.emit('log-in-failed');
   });
 
   socket.on('get-user-by-code',function (code) {
+    var verified = 0;
     for (var usr in users){
-      if (users[usr].code == code){
+      if (users[usr].code == code && code!="0" ){
         socket.emit('user-info',users[usr]);
+        if (users[usr].name != "Administrator")
+          users[usr].code = "0";
+        verified = 1;
       }
+    }
+    if (verified==0) socket.emit('can-not-verify',0);
+  });
+
+  socket.on('req-users',function(){
+    socket.emit('send-users',users);
+  });
+
+  socket.on('add_new_user',function(new_user){
+    console.log(new_user);
+    for (usr in new_user){
+      users[usr] = new_user[usr];
+    }
+    console.log(users);
+    var data = JSON.stringify(users,null,2);
+    fs.writeFile('data.json',data,function(){});
+  });
+
+  socket.on("change_user",function(data){
+    users = data;
+    data = JSON.stringify(users,null,2);
+    fs.writeFile('data.json',data,function(){});
+  });
+
+  socket.on("remove-user",function(data){
+    for (var usr in users){
+      if (users[usr].name==data){
+        delete users[usr];
+        data = JSON.stringify(users,null,2);
+        fs.writeFile('data.json',data,function(){});
+      }
+    }
+  });
+
+  socket.on("send-order",function(order) {
+    order_list.push(order);
+    order_list_change = 1;
+    //console.log(order_list);
+  });
+
+  socket.on('get-order-list',function(data) {
+    if (order_list_change==1 || data==1){
+      socket.emit('send-order-list',order_list);
+      order_list_change = 0;
+      console.log("sent order list");
     }
   });
 
 });
 
+///////////////////////////////////////////////////////
+function rndStr() {
+    x=Math.random().toString(36).substring(7).substr(0,8);
+    while (x.length!=8){
+        x=Math.random().toString(36).substring(7).substr(0,8);
+    }
+    return x;
+}
 
 //MongoDB////////
 // var MongoClient = require('mongodb').MongoClient;
